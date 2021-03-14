c078837335c1af9c2f4fb55b2ba76b390474c9fa
joao <joao.camargo@gmail.com>
2021-03-14 13:22:04 -0300

    teste incial

---
+++ PlaygroundFrameworks/PlaygroundFrameworks.h
@@ -0,0 +1,18 @@
+//
+//  PlaygroundFrameworks.h
+//  PlaygroundFrameworks
+//
+//  Created by joao camargo on 14/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaygroundFrameworks.
+FOUNDATION_EXPORT double PlaygroundFrameworksVersionNumber;
+
+//! Project version string for PlaygroundFrameworks.
+FOUNDATION_EXPORT const unsigned char PlaygroundFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundFrameworks/PublicHeader.h>
+
+
